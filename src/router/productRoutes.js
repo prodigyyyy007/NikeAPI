f1189e673bf3c40c35e0b494c2684e6c1e4177e1
Nirmalya Nayak <n.nayak263@gmail.com>
2023-07-01 13:26:37 +0530

    get all products from db

--- src/router/productRoutes.js
+++ src/router/productRoutes.js
@@ -1,12 +1,22 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("Get all products");
+const { getAllProducts, getProduct } = require("../database/products");
+
+router.get("/", async (req, res) => {
+  const products = await getAllProducts();
+  res.send({ status: "OK", data: products });
 });
 
-router.get("/:productId", (req, res) => {
-  res.send(`Get product with id: ${req.params.productId}`);
-});
+// router.get("/:productId", async (req, res) => {
+//   const product = await getProduct(req.params.productId);
+
+//   if (!product) {
+//     res.status(404).send({ status: "FAILED", error: "Product not found" });
+//     return;
+//   }
+
+//   res.send({ status: "OK", data: product });
+// });
 
 module.exports = router;
