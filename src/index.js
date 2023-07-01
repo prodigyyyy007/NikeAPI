a71c09fe1af85efd4d0598a60cf63123854e3bfa
Nirmalya Nayak <n.nayak263@gmail.com>
2023-07-01 13:44:29 +0530

    added bodyParser middleware

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+
 const productRoutes = require("./router/productRoutes");
 const orderRoutes = require("./router/orderRoutes");
 
 const app = express();
 const PORT = 3000;
 
+app.use(bodyParser.json());
 app.use("/products", productRoutes);
 app.use("/orders", orderRoutes);
 
