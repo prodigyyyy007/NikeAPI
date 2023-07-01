4b2278f97058df2ad07504e4a7c2645b83be16be
Nirmalya Nayak <n.nayak263@gmail.com>
2023-07-01 13:13:45 +0530

    added func to connect to mongodb atlas

--- src/database/db.js
+++ src/database/db.js
@@ -1,2 +1,26 @@
 require("dotenv").config();
-console.log(process.env.DATABASE_URL);
+const { MongoClient } = require("mongodb");
+
+const uri = process.env.DATABASE_URL;
+
+let client;
+
+const getDB = () => {
+  if (!client) {
+    console.log("Creating a new client!");
+    client = new MongoClient(uri);
+  } else {
+    console.log("Reusing the old client");
+  }
+
+  const database = client.db("nike-api");
+  const products = database.collection("products");
+  const orders = database.collection("orders");
+
+  return {
+    products,
+    orders,
+  };
+};
+
+module.exports = getDB;
