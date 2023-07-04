9396ed08bd88ce32ddb6758a343ff3058bf3de86
Nirmalya Nayak <n.nayak263@gmail.com>
2023-07-04 12:08:37 +0530

    added route intents to hande payment

--- src/router/paymentRoutes.js
+++ src/router/paymentRoutes.js
@@ -2,4 +2,26 @@ require("dotenv").config();
 const express = require("express");
 const router = express.Router();
 
+const STRIPE_SECRET_KEY = process.env.STRIPE_SECRET_KEY;
+
+const stripe = require("stripe")(STRIPE_SECRET_KEY);
+
+router.post("/intents", async (req, res) => {
+  try {
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: req.body.amount,
+      currency: "usd",
+      automatic_payment_methods: {
+        enabled: true,
+      },
+    });
+
+    res.json({ paymentIntent: paymentIntent.client_secret });
+  } catch (error) {
+    res.status(400).json({
+      error: error.message,
+    });
+  }
+});
+
 module.exports = router;
