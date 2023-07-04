a0d49ffb600cc066cab1a990bc21a46cf142960d
Nirmalya Nayak <n.nayak263@gmail.com>
2023-07-04 12:01:00 +0530

    added paymentRoutes to app index

--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ const bodyParser = require("body-parser");
 
 const productRoutes = require("./router/productRoutes");
 const orderRoutes = require("./router/orderRoutes");
+const paymentRoutes = require("./router/paymentRoutes");
 
 const app = express();
 const PORT = 3000;
 
 app.use(bodyParser.json());
+
 app.use("/products", productRoutes);
 app.use("/orders", orderRoutes);
+app.use("/payments", paymentRoutes);
 
 app.get("/", (req, res) => {
   res.send("<h2>Hello world!</h2>");
